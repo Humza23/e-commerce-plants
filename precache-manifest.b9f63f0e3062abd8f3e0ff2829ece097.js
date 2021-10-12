655284d74731abd784cd4cd6d7993c6a57760cdc
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-10-12 19:00:08 -0400

    Updates

---
+++ precache-manifest.b9f63f0e3062abd8f3e0ff2829ece097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee84b101511ba0d00fe355ca7db44259",
+    "url": "/e-commerce-plants/index.html"
+  },
+  {
+    "revision": "1852fed27f202a160514",
+    "url": "/e-commerce-plants/static/css/main.66d197e0.chunk.css"
+  },
+  {
+    "revision": "1af83c5f4e31b475392e",
+    "url": "/e-commerce-plants/static/js/2.77f360a5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/e-commerce-plants/static/js/2.77f360a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1852fed27f202a160514",
+    "url": "/e-commerce-plants/static/js/main.5312ec36.chunk.js"
+  },
+  {
+    "revision": "fce74ae2e0e14e80752e",
+    "url": "/e-commerce-plants/static/js/runtime-main.8dbf225b.js"
+  }
+]);
